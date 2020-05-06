7d7dad6a0b7ab64c93dc46dccccd70ed11462533
TimTen10 <Tim.Eckle94@gmail.com>
2020-05-06 21:55:11 +0200

    Finished kmeans

--- src/clustering/methods/kmeans.py
+++ src/clustering/methods/kmeans.py
@@ -1,18 +1,37 @@
-# kmeans returns centroids
 # TODO: Where to put Kosten eines Clusters and Kosten des Clustering
+from src.clustering.util import distances as dist
 
 
-def _generate_clusters(centroids, points):
-    pass
+def _generate_clusters(centroids, points, d):
+    clusters = [[] for _ in centroids]
+    for point in points:
+        distances = [d(centroid, point) for centroid in centroids]
+        clusters[distances.index(min(distances))].append(point)
+    return clusters
 
 
-def kmeans(points, k):
+def _generate_centroid(cluster):
+    return [sum(p[0] for p in cluster) / len(cluster), sum(p[1] for p in cluster) / len(cluster)]
+
+
+def kmeans(points, k, d):
+    # TODO: implementation of uni course kmeans, not quite real kmeans yet! Initial centroids differ
+    # normally split points and generate centroids out of these
+
     centroids = points[:k]
-    clusters = _generate_clusters(centroids, points[k:])
+    # clusters = _generate_clusters(centroids, points, d)
+    final_centroids = list()
+    while final_centroids != centroids:
+        final_centroids = centroids
+        clusters = _generate_clusters(final_centroids, points, d)
+        print(clusters)
+        centroids = [_generate_centroid(cluster) for cluster in clusters]
+    return final_centroids
 
 
 def main():
-    print(kmeans([[1, 2], [4, 5], [7, 3], [0, 9]], 2))
+    print(kmeans([[1, 5], [6, 2], [8, 1], [3, 5], [2, 4], [2, 6],
+                  [6, 1], [6, 8], [7, 3], [7, 6], [8, 3], [8, 7]], 3, dist.lp_metric))
 
 
 if __name__ == '__main__':
