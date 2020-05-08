5608c0dccfb53d3965f3f8f92a60c5fa9906e1ac
TimTen10 <Tim.Eckle94@gmail.com>
2020-05-08 16:47:04 +0200

    Imporved transition probabilites

--- src/util/bias.py
+++ src/util/bias.py
@@ -9,9 +9,9 @@ def _get_alpha(graph_dict, current_node, last_node, node, p, q):
 
 
 def transition_probabilities(graph_dict, current_node, last_node, p, q):
-    if not last_node:
+    if not last_node or p == q == 1:
         alphas = [1 for neighbor in graph_dict[current_node]]
     else:
-        # distances berechnen und abhängig davon alphas
+        # get alphas based on distances from nodes to the current node
         alphas = [_get_alpha(graph_dict, current_node, last_node, node, p, q) for node in graph_dict[current_node]]
     return alphas
