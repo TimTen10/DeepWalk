e73fc6b581cc88e41cd0d47b1a4a40406100b522
TimTen10 <Tim.Eckle94@gmail.com>
2020-05-12 18:49:43 +0200

    Tree handled

--- src/testall.py
+++ src/testall.py
@@ -1,5 +1,7 @@
 import src.network.graph as g
 from src.word2vec.util import preprocess as pp
+from src.word2vec.treemodel import tree
+from anytree import RenderTree, ContStyle
 import json
 
 
@@ -11,7 +13,7 @@ def main():
 
     # generate walks
     walks = pp.generate_walks(graph, 5, 1)
-
+    print(graph.nodes)
     # it keeps the order, very important! But why again?
     # Only order of graph.nodes plays a role (later on in generate_one_hot)
     for w, n in zip(walks, graph.nodes):
@@ -20,6 +22,16 @@ def main():
     # generate input pairs
     pairs = pp.generate_pairs(walks, 2)
     # print(pairs)
+
+    print("Tree Test")
+    baum = tree.create_tree(list(graph.nodes))
+    """
+    print(RenderTree(baum[0], style=ContStyle))
+    for knoten in baum:
+        if knoten.is_leaf:
+            print(knoten)
+    """
+    print(tree.get_path(baum[92].children[0]))
 
 
 if __name__ == '__main__':
--- src/word2vec/treemodel/tree.py
+++ src/word2vec/treemodel/tree.py
@@ -1,18 +1,41 @@
-import numpy as np
+from anytree import Node, RenderTree, ContStyle, walker
 
 
-class Tree:
+def create_tree(leaves):
+    counter = 0
+    tree = [Node('0')]
+    length = len(leaves)
+    for i in range(1, length + length - 1):
+        if i >= length - 1:
+            tree.append(Node(leaves[i - length + 1], tree[counter]))
+        else:
+            tree.append(Node(f'{i}', tree[counter]))
+        if not i % 2:
+            counter += 1
+    return tree
 
-    def __init__(self, dimensionality, nodes, walks):
-        self.dimensionality = dimensionality
-        self.leaves = nodes
-        self.walks = walks
-        self.frequencies = self._generate_frequencies(walks)
-        self.node_vectors = np.random.uniform(size=(dimensionality, len(nodes) - 1))
-        # self.codes = self._generate_code(self.frequencies)
 
-    def _generate_frequencies(self, walks):
-        normalization = sum(len(walk) for walk in walks)
-        frequencies = {leave: (sum(walk.count(leave) for walk in walks)/normalization) for leave in self.leaves}
-        sorted_frequencies = {k: v for k, v in sorted(frequencies.items(), key=lambda item: item[1])}
-        return sorted_frequencies
+def get_path(node):
+    return [s for s in str(node).split("'")[1].split('/')[1:]]
+
+
+def main():
+    counter = 0
+    tree = [Node("0")]
+    for i in range(1, 127 + 128):
+        if i < 90:
+            tree.append(Node(f"{i}", tree[counter]))
+        else:
+            tree.append(Node(f"{i}", tree[counter]))
+        if not i % 2:
+            counter += 1
+
+    w = walker.Walker()
+    node = str(w.walk(tree[0], tree[127])[-1][-1])
+    for s in node.split("'")[1].split('/')[1:]:
+        print(s)
+    print(tree[127].is_leaf)
+
+
+if __name__ == '__main__':
+    main()
